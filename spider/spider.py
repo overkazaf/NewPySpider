8a984f558eccb3e0432e8fa5eef048a5a775b49f
XSunny <854610556@qq.com>
2016-03-12 23:23:01 +0800

    some spider coding

--- spider/spider.py
+++ spider/spider.py
@@ -2,7 +2,28 @@ import urllib2, urllib, fileUtil, httpClient
 
 _default_parms  = {"User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36", "Referer":"http://www.luoo.net/music/800"}
 
-data  = httpClient.crawlerResource("http://luoo-mp3.kssws.ks-cdn.com/low/luoo/radio800/01.mp3", "GET", None)
 
-fileUtil.saveByteFile("./mp3/1.mp3", data)
+def getMusic(volNumber):
+	#如果出现异常，则退出循环
+	goon = 1
+	while (goon):
+		try:
+			data = httpClient.crawlerResource("http://luoo-mp3.kssws.ks-cdn.com/low/luoo/radio"+volNumber+"/"+mnumber+".mp3", "GET", None)
+			fileUtil.saveByteFile("./"+volNumber+"/mp3/"+mnumber+".mp3", data)
+		except Exception, e:
+			goon = 0
+		else:
+			pass
+		finally:
+			pass
+		
+
+def getThanks(url):
+	pass
+
+
+def getPic(url):
+	pass
+
+
 #print html 
