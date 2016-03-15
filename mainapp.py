1660d5948fae305bda716e5c2689152a2e7c1968
XSunny <854610556@qq.com>
2016-03-16 00:03:16 +0800

    edit appcontroler

--- mainapp.py
+++ mainapp.py
@@ -3,11 +3,18 @@ import spider.spider as Spider
 
 app = Flask(__name__)
 
+#主页
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/user/crawler/config')
+#显示结果
+@app.route('/showResult')
+def showResult():
+    return render_template('home.html')
+
+#爬虫配置
+@app.route('crawler/config')
 def spiderConfig():
 	return Spider.test()
 
